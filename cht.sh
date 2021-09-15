4cdf54bdcbb029b23423f6a712a63be66d791741
Roy Palomino <roy100995@gmail.com>
2021-09-15 11:21:16 -0500

    refactor: no need to bind in tmux

--- cht.sh
+++ cht.sh
@@ -5,8 +5,10 @@ read -p "Enter Query: " query
 
 if grep -qs "$selected" ~/.config/dotfiles/.tmux-cht-languages; then
   query=`echo $query | tr ' ' '+'`
-  tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+  tmux neww zsh -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
 else
-  tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
+  query=`echo $query | tr ' ' '+'`
+  tmux neww zsh -c "echo \"curl cht.sh/$selected~$query/\" & curl cht.sh/$selected~$query & while [ : ]; do sleep 1; done"
+  #tmux neww bash -c "curl -s cht.sh/$selected~$query"
 fi
 bind-key -r i run-shell "tmux neww tmux-cht.sh"
